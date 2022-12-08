88a0678b4a14ac5440ea0abc6966ee082d8db98d
Dmitry Kalugin <n.demitsuri@gmail.com>
2022-12-09 02:33:43 +0300

    Fix areEqual once again to work with arrays of arrays

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const areEqual = (a, b) => {
         }
 
         for (let i = 0; i < a.length; i++) {
-            if (a[i] !== b[i]) {
+            if (!areEqual(a[i], b[i])) {
                 return false;
             }
         }
